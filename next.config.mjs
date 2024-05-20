ff71bbde736580df849279f27444c4f4c9772463
Pavel Dzhalev <dzhalev.pavel@gmail.com>
2024-05-20 13:49:00 +0300

    Changes accroding to GPT

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: '/front', // replace 'your-repo-name' with the name of your GitHub repository
+  assetPrefix: '/front', // replace 'your-repo-name' with the name of your GitHub repository
 };
 
 export default nextConfig;
