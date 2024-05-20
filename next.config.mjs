0e99909bd75a7eebf7bd0727b9f58deff6f2d086
Pavel Dzhalev <dzhalev.pavel@gmail.com>
2024-05-20 12:53:23 +0300

    Output static

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
 
 export default nextConfig;
