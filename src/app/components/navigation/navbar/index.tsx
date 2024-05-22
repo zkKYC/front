3205620bfac88d1f2f41e38f9a214bfc1c3a387e
padjal <dzhalev.pavel@gmail.com>
2024-05-22 18:02:11 +0300

    Change header background

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -6,34 +6,34 @@ import Button from "./Button";
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-20 bg-white sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
-            <ul className="hidden md:flex gap-x-6 text-white">
+            <ul className="hidden md:flex gap-x-6 text-black">
               
               <li>
-                <Link href="/registration">
+                <Link href="/registration" className="px-3 py-2 hover:bg-gray-200 hover:text-black block rounded-md">
                   <p>Registration</p>
                 </Link>
               </li>
               <li>
-                <Link href="/my-sbts">
+                <Link href="/my-sbts" className="px-3 py-2 hover:bg-gray-200 hover:text-black block rounded-md">
                   <p>My SBTs</p>
                 </Link>
               </li>
               <li>
-                <Link href="/commit">
+                <Link href="/commit" className="px-3 py-2 hover:bg-gray-200 hover:text-black block rounded-md">
                   <p>Commit</p>
                 </Link>
               </li>
               <li>
-                <Link href="/check">
+                <Link href="/check" className="px-3 py-2 hover:bg-gray-200 hover:text-black block rounded-md">
                   <p>Check</p>
                 </Link>
               </li>
               <li>
-                <Link href="/proof">
+                <Link href="/proof" className="px-3 py-2 hover:bg-gray-200 hover:text-black block rounded-md">
                   <p>Proof</p>
                 </Link>
               </li>
